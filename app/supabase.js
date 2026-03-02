e72c06096ba890baf6b257bf86c8955ef526b926
aamirbaugwala <aamir.baugwala@issgovernance.com>
2026-03-03 01:17:18 +0530

    isp fix

--- app/supabase.js
+++ app/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://eqfgddpyyymgbcgrkxiy.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxZmdkZHB5eXltZ2JjZ3JreGl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcyNzU4NDAsImV4cCI6MjA4Mjg1MTg0MH0.VUbGyEcGbu77_9LT5btdA9YNXGGrHhA6JJ2oARn6jP0';
-
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 // Custom fetch with retry logic for spotty cellular connections
 const customFetch = async (url, options, retries = 3) => {
